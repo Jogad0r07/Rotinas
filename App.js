98687261b02146daf932a349975de400459e2a99
Cabelinho <hugostoso25nutri@gmail.com>
2024-03-26 14:06:59 -0300

    Adição do component header

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from "react";
+import { Text, StyleSheet, View, Image } from "react-native";
+import Header from "./components/header";
+import Card from "./components/card";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={estilo.container}>
+      <Header></Header>
+      <Card
+        porcentagem="5.95%"
+        titulo="Transporte Ida e Volta para a escola"
+        imagem='5.png'
+        descricao="Segunda a Sexta"
+      ></Card>
+      <Card
+        porcentagem="41.67%"
+        titulo="Dormir"
+        imagem="2.png"
+        descricao="Todos os dias"
+      ></Card>
+      <Card
+        porcentagem="15.48%"
+        titulo="Etec"
+        imagem="3.png"
+        descricao="Segunda a Sexta"
+      ></Card>
+      <Card
+        porcentagem="8.33%"
+        titulo="Almoço e Janta"
+        imagem="1.png"
+        descricao="Todos os dias"
+      ></Card>
+      <Card
+        porcentagem="41.67%"
+        titulo="Arrumar e Organizar a casa"
+        imagem="6.png"
+        descricao="Todos os dias"
+      ></Card>
+
     </View>
   );
 }
-
-const styles = StyleSheet.create({
+const estilo = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    backgroundColor: "#CAF0F8",
+    borderColor: "#90E0EF",
+    paddingHorizontal: 20,
   },
 });
