1a7e8335ec43b47ff4cf55a28671d83f8e360d29
Gabriel <gp.alves10@gmail.com>
2024-04-09 14:36:36 -0300

    Adição do DATA

---
+++ data/index.js
@@ -0,0 +1,40 @@
+const DATA = 
+[
+    {
+      id: '01',
+      porcentagem: '5.95%',
+      titulo="Transporte Ida e Volta para a escola",
+      imagem='5.png',
+      descricao="Segunda a Sexta",
+    },
+    {
+        id: '02',
+        porcentagem: '43.67%',
+        titulo: 'Dormir',
+        imagem: '2.png',
+        descricao: 'Todos os dias'
+    },
+    {
+        id: '03',
+        porcentagem: '15.48%',
+        titulo: 'Etec',
+        imagem: '3.png',
+        descricao: 'Segunda a Sexta'
+    },
+    {
+        id: '04',
+        porcentagem: '8.33%',
+        titulo: 'Almoço e Janta',
+        imagem: '1.png',
+        descricao: 'Todos os Dias'
+    },
+    {
+        id: '05',
+        porcentagem: '26.57%',
+        titulo: 'Arrumar e Organizar a casa',
+        imagem: '6.png',
+        descricao: 'Todos os Dias'
+    },
+]
+
+export default DATA;
